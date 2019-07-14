3694b84c65577e5c9728e13cd4157a38ba1997e2
yangzhi <yangzhi@outlook.com>
2019-07-14 10:19:00 +0800

    add files

---
+++ Pythonnet/codeandnote/day8all/multi_process.py
@@ -0,0 +1,14 @@
+import multiprocessing as mp
+import time 
+
+jobs = []
+t = time.time()
+for i in range(10):
+    p = mp.Process(target=io)
+    jobs.append(p)
+    p.start()
+
+for i in jobs:
+    i.join()
+
+print("Process IO:",time.time() - t)
